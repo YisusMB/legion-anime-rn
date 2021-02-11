5bd89855ed6ede2d17c4c5138c5be5b522b2de83
Jesus Moreno <jesus@evenlift.io>
2021-02-10 19:57:43 -0600

    [ADDED] React Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import { Button } from 'react-native';
 
-export default function App() {
+// Here goes all screen props for stack navigator
+type RootStackParamList = {
+    Home: undefined;
+    Details: { animeId: number, image: string | number, lastSeenEpisode: number | null };
+};
+
+type HomeScreenNavigationProp =  StackNavigationProp<RootStackParamList, 'Home'>;
+type DetailsScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Details'>
+type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
+
+
+type HomeProps = {
+    navigation: HomeScreenNavigationProp;
+};
+
+type DetailsProps = {
+    navigation: DetailsScreenNavigationProp;
+    route: DetailsScreenRouteProp;
+}
+
+function HomeScreen({ navigation }: HomeProps) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details', { animeId: 1, image: 'hi', lastSeenEpisode: null })}
+            />
+        </View>
+    );
+}
+
+function DetailsScreen({ navigation, route }: DetailsProps) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+            <Text>{JSON.stringify(route.params)}</Text>
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
